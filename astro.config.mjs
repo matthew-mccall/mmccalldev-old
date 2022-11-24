e1ec3f291007b4d9feb6fa1454a246763267d046
Matthew McCall <matthew+github@mmccall.dev>
2022-11-24 14:52:06 +0000

    Added blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [react()]
+  integrations: [react(), mdx(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
--- src/components/VCenter.tsx
+++
@@ -1,9 +0,0 @@
-export default function VCenter(props: { children?: React.ReactNode }) {
-    return (
-        <div className="row h-100">
-            <div className="col my-auto">
-                {props.children}
-            </div>
-        </div>
-    )
-}
